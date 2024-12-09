056d81b20da35d8a8765959ed1839bd6c062ea58
EpocDotFr <contact@epoc.fr>
2024-12-09 23:11:14 +0100

    Ajout des routes de base

---
+++ resources/views/site/lan.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>LAN</title>
+</head>
+<body>
+    <p>LAN</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::name('web.')
+    ->group(function () {
+        Route::domain('team-lan.test')
+            ->name('site.')
+            ->group(function () {
+                Route::view('/', 'site.home')->name('home');
+                Route::view('/lan', 'site.lan')->name('lan');
+            });
+
+        Route::domain('hub.team-lan.test')
+            ->name('hub.')
+            ->group(function () {
+                Route::view('/', 'hub.home')->name('home');
+            });
+    });
